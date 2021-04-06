57c1c7b6cafd99c26b0307690c11340815d5d23c
Taha Akhtar <taha-akhtar13@hotmail.com>
2021-04-06 12:38:24 +0400

    remove mongo

--- app.js
+++ app.js
@@ -13,21 +13,21 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 
-var dbURI = 'mongodb://localhost:27017/NodeAuth?readPreference=primary&appname=nodeauth';
+// var dbURI = 'mongodb://localhost:27017/NodeAuth?readPreference=primary&appname=nodeauth';
 
-mongoose
-    .connect(dbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-})
-    .then((result) => {
-        //        console.log('coonected to db')
-      //  app.listen(3000);
-    })
-    .catch((err) => {
-        console.log(err)
-    });
+// mongoose
+//     .connect(dbURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true
+// })
+//     .then((result) => {
+//         //        console.log('coonected to db')
+//       //  app.listen(3000);
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     });
 
 app.get('/logout', requireAuth, (req, res) => {
     res.cookie('jwt-auth', '', {maxAge: 1});
